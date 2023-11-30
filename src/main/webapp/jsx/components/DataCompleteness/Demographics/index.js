a463b3caacb1c3318a01405ff33f7d3ddabfed3b
vixxjy <ajorvictor48@gmail.com>
2023-11-30 20:29:59 +0100

    [DQA] Frontend Demographics added

--- src/main/webapp/jsx/components/DataCompleteness/Demographics/index.js
+++ src/main/webapp/jsx/components/DataCompleteness/Demographics/index.js
@@ -1,289 +1,223 @@
-import React from "react";
-
-import {Form, Table } from "reactstrap";
-import {makeStyles} from "@material-ui/core/styles";
-import {Card, CardContent} from "@material-ui/core";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { token, url as baseUrl } from "../../../../api";
+import { Form, Table } from "reactstrap";
+import { makeStyles } from "@material-ui/core/styles";
+import { Card, CardContent } from "@material-ui/core";
 
 import "react-toastify/dist/ReactToastify.css";
 import "react-widgets/dist/css/react-widgets.css";
 
-
-
 const useStyles = makeStyles((theme) => ({
-    card: {
-        margin: theme.spacing(20),
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center"
+  card: {
+    margin: theme.spacing(20),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  cardBottom: {
+    marginBottom: 20,
+  },
+  Select: {
+    height: 45,
+    width: 300,
+  },
+  button: {
+    margin: theme.spacing(1),
+  },
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
     },
-    form: {
-        width: "100%", // Fix IE 11 issue.
-        marginTop: theme.spacing(3),
+    "& .card-title": {
+      color: "#fff",
+      fontWeight: "bold",
     },
-    submit: {
-        margin: theme.spacing(3, 0, 2),
+    "& .form-control": {
+      borderRadius: "0.25rem",
+      height: "41px",
     },
-    cardBottom: {
-        marginBottom: 20,
+    "& .card-header:first-child": {
+      borderRadius: "calc(0.25rem - 1px) calc(0.25rem - 1px) 0 0",
     },
-    Select: {
-        height: 45,
-        width: 300,
+    "& .dropdown-toggle::after": {
+      display: " block !important",
     },
-    button: {
-        margin: theme.spacing(1),
+    "& select": {
+      "-webkit-appearance": "listbox !important",
     },
-    root: {
-        '& > *': {
-            margin: theme.spacing(1)
-        },
-        "& .card-title":{
-            color:'#fff',
-            fontWeight:'bold'
-        },
-        "& .form-control":{
-            borderRadius:'0.25rem',
-            height:'41px'
-        },
-        "& .card-header:first-child": {
-            borderRadius: "calc(0.25rem - 1px) calc(0.25rem - 1px) 0 0"
-        },
-        "& .dropdown-toggle::after": {
-            display: " block !important"
-        },
-        "& select":{
-            "-webkit-appearance": "listbox !important"
-        },
-        "& p":{
-            color:'red'
-        },
-        "& label":{
-            fontSize:'14px',
-            color:'#014d88',
-            fontWeight:'bold'
-        }
+    "& p": {
+      color: "red",
     },
-    demo: {
-        backgroundColor: theme.palette.background.default,
+    "& label": {
+      fontSize: "14px",
+      color: "#014d88",
+      fontWeight: "bold",
     },
-    inline: {
-        display: "inline",
-    },
-    error:{
-        color: '#f85032',
-        fontSize: '12.8px'
-    },
-    success: {
-        color: "#4BB543 ",
-        fontSize: "11px",
-    },
+  },
+  demo: {
+    backgroundColor: theme.palette.background.default,
+  },
+  inline: {
+    display: "inline",
+  },
+  error: {
+    color: "#f85032",
+    fontSize: "12.8px",
+  },
+  success: {
+    color: "#4BB543 ",
+    fontSize: "11px",
+  },
 }));
 
-
 const DemographicsDQA = (props) => {
-    const classes = useStyles();
+  const classes = useStyles();
+  const [demographics, setDemographic] = useState({});
+  const [facilities, setFacilities] = useState([]);
 
+  const Facilities = () => {
+    axios
+      .get(`${baseUrl}account`, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((response) => {
+        setFacilities(response.data.currentOrganisationUnitId);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-    return (
-        <>
-           
-            <Card className={classes.root}>
-                <CardContent>
-                    <h3>Demographics Variables</h3>
-                    <div className="col-xl-12 col-lg-12">
-                        <Table bordered>
-                            <thead>
-                            <tr>
-                                <th>
-                                    #
-                                </th>
-                                <th>
-                                    Complete Variables
-                                </th>
-                                <th>
-                                    Numerator
-                                </th>
-                                <th>
-                                  Denominator
-                                </th>
-                                <th>
-                                  Performance
-                                </th>
-                                <th>
-                                    Action
-                                </th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <tr>
-                                <th scope="row">
-                                    1
-                                </th>
-                                <td>
-                                    Proportion of all active patients with Date of Birth (DOB)
-                                </td>
-                                <td>
+  const loadDemography = () => {
+    axios
+      .get(`${baseUrl}dqr/dqa-dob-summary?facilityId=${facilities}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((response) => {
+        setDemographic(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-                                </td>
-                                <td>
-                                    
-                                </td>
-                                <td>
-                                    
-                                </td>
-                                <td>
-                                    
-                                </td>
-                            </tr>
-                            <tr>
-                                <th scope="row">
-                                    2
-                                </th>
-                                <td>
-                                    Proportion of all active patients with Current Age
-                                </td>
-                                <td>
-                                    
-                                </td>
-                                <td>
-                                    
-                                </td>
-                                <td>
-                                    
-                                </td>
-                                <td>
-                                    
-                                </td>
-                            </tr>
-                            <tr>
-                                <th scope="row">
-                                    3
-                                </th>
-                                <td>
-                                    Proportion of all active patients with Patient Identifier
-                                </td>
-                                <td>
-                                    
-                                </td>
-                                <td>
-                                    
-                                </td>
-                                <td>
-                                    
-                                </td>
-                                <td>
-                                    
-                                </td>
-                            </tr>
-                            <tr>
-                                <th scope="row">
-                                    4
-                                </th>
-                                <td>
-                                    Proportion of all active patients with Sex
-                                </td>
-                                <td>
-                                    
-                                </td>
-                                <td>
-                                    
-                                </td>
-                                <td>
-                                    
-                                </td>
-                                <td>
-                                    
-                                </td>
-                            </tr>
-                            <tr>
-                                <th scope="row">
-                                    5
-                                </th>
-                                <td>
-                                    Proportion of all active patients with a documented educational Status
-                                </td>
-                                <td>
-                                    
-                                </td>
-                                <td>
-                                    
-                                </td>
-                                <td>
-                                    
-                                </td>
-                                <td>
-                                    
-                                </td>
-                            </tr>
-                            <tr>
-                                <th scope="row">
-                                    6
-                                </th>
-                                <td>
-                                    Proportion of all active patients with Date of Birth (DOB)
-                                </td>
-                                <td>
-                                    
-                                </td>
-                                <td>
-                                    
-                                </td>
-                                <td>
-                                    
-                                </td>
-                                <td>
-                                    
-                                </td>
-                            </tr>
-                            <tr>
-                                <th scope="row">
-                                    7
-                                </th>
-                                <td>
-                                    Proportion of all active patients with a documented marital status
-                                </td>
-                                <td>
-                                    
-                                </td>
-                                <td>
-                                    
-                                </td>
-                                <td>
-                                    
-                                </td>
-                                <td>
-                                    
-                                </td>
-                            </tr>
-                            <tr>
-                                <th scope="row">
-                                    8
-                                </th>
-                                <td>
-                                Proportion of all active patients with registered address/LGA of residence
-                                </td>
-                                <td>
-                                    
-                                </td>
-                                <td>
-                                    
-                                </td>
-                                <td>
-                                    
-                                </td>
-                                <td>
-                                    
-                                </td>
-                            </tr>
+  useEffect(() => {
+    Facilities();
+    loadDemography();
+  }, []);
 
-                            </tbody>
-                        </Table>
-                    </div>
-                </CardContent>
-            </Card>
-
-        </>
-    );
+  return (
+    <>
+      <Card className={classes.root}>
+        <CardContent>
+          <h3>Demographics Variables</h3>
+          <div className="col-xl-12 col-lg-12">
+            <Table bordered>
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Complete Variables</th>
+                  <th>Numerator</th>
+                  <th>Denominator</th>
+                  <th>Performance</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">1</th>
+                  <td>
+                    Proportion of all active patients with Date of Birth (DOB)
+                  </td>
+                  <td>{demographics[0]?.numerator}</td>
+                  <td>{demographics[0]?.denominator}</td>
+                  <td>{demographics[0]?.performance}%</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <th scope="row">2</th>
+                  <td>Proportion of all active patients with Current Age</td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <th scope="row">3</th>
+                  <td>
+                    Proportion of all active patients with Patient Identifier
+                  </td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <th scope="row">4</th>
+                  <td>Proportion of all active patients with Sex</td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <th scope="row">5</th>
+                  <td>
+                    Proportion of all active patients with a documented
+                    educational Status
+                  </td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <th scope="row">6</th>
+                  <td>
+                    Proportion of all active patients with Date of Birth (DOB)
+                  </td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <th scope="row">7</th>
+                  <td>
+                    Proportion of all active patients with a documented marital
+                    status
+                  </td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <th scope="row">8</th>
+                  <td>
+                    Proportion of all active patients with registered
+                    address/LGA of residence
+                  </td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+              </tbody>
+            </Table>
+          </div>
+        </CardContent>
+      </Card>
+    </>
+  );
 };
 
-export default DemographicsDQA
+export default DemographicsDQA;
